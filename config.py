a3f63f5153305298c763f28dc2c4f49553a752c7
Elfreezy <bloody.sekret@yandex.ru>
2020-08-08 17:24:50 +0300

    ref

--- config.py
+++ config.py
@@ -8,15 +8,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
-    SQLALCHEMY_DATABASE_URI = '{db_type}+{driver_name}://{user}:{password}@{host}/{name}'.format(
-        db_type=os.environ.get('DB_TYPE'),
-        driver_name=os.environ.get('DRIVER_NAME'),
-        user=os.environ.get('USER'),
-        password=os.environ.get('PASSWORD'),
-        host=os.environ.get('HOST'),
-        port=os.environ.get('PORT'),
-        name=os.environ.get('DB_NAME'),
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CLIENT_ID = os.environ.get('CLIENT_ID')
